8ee5d6a82212d569c53a5e835d538afbb4ef92cc
Nguyen Phuoc Hung <zakizika1590623847@gmail.com>
2024-09-18 23:12:19 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const cors = require("cors");
 
 const app = express();
 
+const contactsRouter = require("./app/routes/contact.route");
+
+app.use("/api/contacts", contactsRouter);
+
 app.use(cors());
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcom to contact book application." });
